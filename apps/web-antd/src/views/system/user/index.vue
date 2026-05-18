068d78b7508a7342bc279f64f27726ae8edc05c9
thence <2668812066@qq.com>
2026-05-18 22:16:56 +0800

    feat: user manage module open export config

--- apps/web-antd/src/views/system/user/index.vue
+++ apps/web-antd/src/views/system/user/index.vue
@@ -47,7 +47,7 @@ const [Grid, gridApi] = useVbenVxeGrid({
     },
     toolbarConfig: {
       custom: true,
-      export: false,
+      export: true,
       refresh: true,
       search: true,
       zoom: true,
